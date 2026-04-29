332a396a5daeedf75a4ab63ee318857227c26ab9
Quan Tran Huynh <qtranhuynh@stetson.edu>
2026-04-29 17:36:35 -0400

    game fixed

--- game/battlewindow.go
+++ game/battlewindow.go
@@ -16,23 +16,20 @@ type combatantSnapshot struct {
 
 type BattleWindow struct {
 	core.BaseWindow[Game]
-
-	enemyTexture rl.Texture2D
 }
 
 var (
-	battleBgColor        = rl.Red
-	battlePanelColor     = rl.NewColor(20, 28, 42, 245)
-	battleInsetColor     = rl.NewColor(28, 38, 56, 255)
-	battleAccentColor    = rl.NewColor(94, 176, 239, 255)
-	battleBorderColor    = rl.NewColor(188, 212, 230, 235)
-	battleEnemyGlowColor = rl.NewColor(167, 79, 48, 255)
+	battleBgColor        = rl.Black
+	battlePanelColor     = rl.Black
+	battleInsetColor     = rl.Black
+	battleAccentColor    = rl.Gray
+	battleBorderColor    = rl.White
+	battleEnemyGlowColor = rl.Gray
 )
 
 func NewBattleWindow(sizeFunc func(gm *core.GameManager) rl.Rectangle, gm *core.GameManager) *BattleWindow {
 	return &BattleWindow{
-		BaseWindow:   core.NewBaseWindow[Game](sizeFunc, gm, true),
-		enemyTexture: rl.LoadTexture("assets/textures/battle/system/medbay.png"),
+		BaseWindow: core.NewBaseWindow[Game](sizeFunc, gm, true),
 	}
 }
 
@@ -61,9 +58,16 @@ func fitTextSize(text string, maxWidth float32, preferredSize int32, minSize int
 	return minSize
 }
 
-func drawFittedText(text string, x int32, y int32, maxWidth float32, preferredSize int32, minSize int32, color rl.Color) int32 {
+func drawFittedText(gm *core.GameManager, text string, x int32, y int32, maxWidth float32, preferredSize int32, minSize int32, color rl.Color) int32 {
 	textSize := fitTextSize(text, maxWidth, preferredSize, minSize)
-	rl.DrawText(text, x, y, textSize, color)
+	rl.DrawTextEx(
+		gm.Fonts["ec"],
+		text,
+		rl.NewVector2(float32(x), float32(y)),
+		float32(textSize),
+		2,
+		color,
+	)
 	return textSize
 }
 
@@ -83,9 +87,7 @@ func drawSectionHeader(bounds rl.Rectangle, title string, accent rl.Color) {
 	rl.DrawText(title, int32(bounds.X+12), int32(bounds.Y+8), fitTextSize(title, bounds.Width-24, 18, 14), rl.White)
 }
 
-func (b BattleWindow) drawEnemySidebar(sidebarBounds rl.Rectangle, snapshot combatantSnapshot) {
-	drawPanelCard(sidebarBounds, battlePanelColor, rl.Fade(battleBorderColor, 0.85))
-
+func (b BattleWindow) drawEnemySidebar(gm *core.GameManager, sidebarBounds rl.Rectangle, snapshot combatantSnapshot) {
 	padding := float32(12)
 	barHeight := float32(24)
 	barGap := float32(10)
@@ -95,29 +97,36 @@ func (b BattleWindow) drawEnemySidebar(sidebarBounds rl.Rectangle, snapshot comb
 		Width:  sidebarBounds.Width - padding*2,
 		Height: 56,
 	}
-	drawPanelCard(headerBounds, battleInsetColor, rl.Fade(battleEnemyGlowColor, 0.9))
-	rl.DrawRectangleRec(rl.Rectangle{
-		X:      headerBounds.X,
-		Y:      headerBounds.Y,
-		Width:  5,
-		Height: headerBounds.Height,
-	}, battleEnemyGlowColor)
 
 	textX := int32(headerBounds.X + 12)
 	textY := int32(headerBounds.Y + 8)
 	textWidth := headerBounds.Width - 24
 	titleSize := fitTextSize(snapshot.Name, textWidth, 26, 14)
-	rl.DrawText(snapshot.Name, textX, textY, titleSize, rl.White)
+	rl.DrawTextEx(
+		gm.Fonts["ec"],
+		snapshot.Name,
+		rl.NewVector2(float32(textX), float32(textY)),
+		float32(titleSize),
+		2,
+		rl.White,
+	)
 
 	statusText := "Status: Active"
-	statusColor := rl.White
+	statusColor := rl.LightGray
 	if !snapshot.Alive {
 		statusText = "Status: Defeated"
 		statusColor = rl.Yellow
 	}
 	statusY := textY + titleSize + 4
 	statusSize := fitTextSize(statusText, textWidth, 18, 12)
-	rl.DrawText(statusText, textX, statusY, statusSize, statusColor)
+	rl.DrawTextEx(
+		gm.Fonts["ec"],
+		statusText,
+		rl.NewVector2(float32(textX), float32(statusY)),
+		float32(statusSize),
+		2,
+		statusColor,
+	)
 
 	imageWidth := sidebarBounds.Width - padding*2
 	statsHeight := barHeight*3 + barGap*2 + 14
@@ -133,10 +142,9 @@ func (b BattleWindow) drawEnemySidebar(sidebarBounds rl.Rectangle, snapshot comb
 		Width:  imageWidth,
 		Height: imageHeight,
 	}
-	drawPanelCard(imageBounds, rl.NewColor(34, 28, 30, 255), rl.Fade(battleEnemyGlowColor, 0.9))
 
-	textureWidth := float32(b.enemyTexture.Width)
-	textureHeight := float32(b.enemyTexture.Height)
+	textureWidth := float32(gm.Textures["enemy"].Width)
+	textureHeight := float32(gm.Textures["enemy"].Height)
 	drawBounds := imageBounds
 	if textureWidth > 0 && textureHeight > 0 {
 		textureAspect := textureWidth / textureHeight
@@ -150,109 +158,32 @@ func (b BattleWindow) drawEnemySidebar(sidebarBounds rl.Rectangle, snapshot comb
 		}
 	}
 
-	rl.DrawTexturePro(
-		b.enemyTexture,
-		rl.Rectangle{
-			X:      0,
-			Y:      0,
-			Width:  float32(b.enemyTexture.Width),
-			Height: float32(b.enemyTexture.Height),
-		},
-		drawBounds,
-		rl.Vector2{},
-		0,
-		rl.White,
-	)
-
-	barX := sidebarBounds.X + padding
-	barWidth := sidebarBounds.Width - padding*2
-	statsBounds := rl.Rectangle{
-		X:      barX,
-		Y:      imageBounds.Y + imageBounds.Height + 10,
-		Width:  barWidth,
-		Height: sidebarBounds.Y + sidebarBounds.Height - padding - (imageBounds.Y + imageBounds.Height + 10),
-	}
-	drawPanelCard(statsBounds, battleInsetColor, rl.Fade(battleEnemyGlowColor, 0.6))
-
-	healthBarY := statsBounds.Y + 10
-	drawStatBar(
-		rl.Rectangle{
-			X:      barX,
-			Y:      healthBarY,
-			Width:  barWidth,
-			Height: barHeight,
-		},
-		"Health",
-		snapshot.Health,
-		snapshot.MaxHealth,
-		rl.Red,
-		18,
-	)
-
-	damageBarY := healthBarY + barHeight + barGap
-	maxDamageDisplay := snapshot.AttackPower
-	if maxDamageDisplay < 5 {
-		maxDamageDisplay = 5
-	}
-	drawStatBar(
-		rl.Rectangle{
-			X:      barX,
-			Y:      damageBarY,
-			Width:  barWidth,
-			Height: barHeight,
-		},
-		"Attack",
-		snapshot.AttackPower,
-		maxDamageDisplay,
-		rl.Orange,
-		18,
-	)
-
-	cooldownBarY := damageBarY + barHeight + barGap
-	drawStatBar(
-		rl.Rectangle{
-			X:      barX,
-			Y:      cooldownBarY,
-			Width:  barWidth,
-			Height: barHeight,
-		},
-		"Shields",
-		snapshot.ShieldLayers,
-		4,
-		rl.SkyBlue,
-		18,
-	)
+	rl.DrawTexture(gm.Textures["enemyF"], 0, 0, rl.White)
 }
 
 func (b BattleWindow) DrawWindow(gm *core.GameManager, state *Game) {
-	bounds := b.GetBounds()
-	rl.DrawRectangleRec(bounds, battleBgColor)
-	rl.DrawRectangleLinesEx(bounds, 2, rl.Fade(battleBorderColor, 0.6))
-
-	padding := float32(12)
 	enemyPanel := rl.Rectangle{
-		X:      bounds.X + padding,
-		Y:      bounds.Y + padding,
-		Width:  bounds.Width - padding*2,
-		Height: bounds.Height - padding*2,
+		X:      544,
+		Y:      360,
+		Width:  192,
+		Height: 208,
 	}
 
 	if state.Enemy == nil {
 		drawPanelCard(enemyPanel, battlePanelColor, rl.Fade(battleBorderColor, 0.85))
-		drawFittedText("No enemy in range", int32(enemyPanel.X+12), int32(enemyPanel.Y+16), enemyPanel.Width-24, 28, 16, rl.White)
-		drawFittedText("Complete mission goals to spawn the next hostile.", int32(enemyPanel.X+12), int32(enemyPanel.Y+50), enemyPanel.Width-24, 16, 11, rl.LightGray)
-		return
+		drawFittedText(gm, "No enemy in range", int32(enemyPanel.X+12), int32(enemyPanel.Y+16), enemyPanel.Width-24, 28, 16, rl.White)
+		drawFittedText(gm, "Complete mission goals to spawn the next hostile.", int32(enemyPanel.X+12), int32(enemyPanel.Y+50), enemyPanel.Width-24, 16, 11, rl.LightGray)
+	} else {
+		enemySnapshot := combatantSnapshot{
+			Name:         state.Enemy.Name(),
+			Health:       state.Enemy.Health(),
+			MaxHealth:    state.Enemy.MaxHealth(),
+			ShieldLayers: state.Enemy.ShieldLayers(),
+			AttackPower:  state.Enemy.Attack(),
+			Alive:        state.Enemy.Alive(),
+		}
+		b.drawEnemySidebar(gm, enemyPanel, enemySnapshot)
 	}
-
-	enemySnapshot := combatantSnapshot{
-		Name:         state.Enemy.Name(),
-		Health:       state.Enemy.Health(),
-		MaxHealth:    state.Enemy.MaxHealth(),
-		ShieldLayers: state.Enemy.ShieldLayers(),
-		AttackPower:  state.Enemy.Attack(),
-		Alive:        state.Enemy.Alive(),
-	}
-	b.drawEnemySidebar(enemyPanel, enemySnapshot)
 
 	rl.DrawTexture(gm.Textures["terminal"], 0, 0, rl.White)
 	rl.DrawTexture(gm.Textures["layout"], 0, 0, rl.White)
@@ -281,4 +212,7 @@ func (b BattleWindow) DrawWindow(gm *core.GameManager, state *Game) {
 		rl.Color{R: 0, G: 0, B: 0, A: 200},
 		rl.Color{R: 0, G: 0, B: 0, A: 0},
 	)
+}
+
+func (b BattleWindow) DrawWindowUI(gm *core.GameManager, state *Game) {
 }
